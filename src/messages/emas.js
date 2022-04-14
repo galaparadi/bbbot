add7e97ec055345b425b709b5fb2f4e74fdcf1a5
galaparadi <gala_koyin@yahoo.co.id>
2022-04-14 08:59:02 +0700

    reffactor mesage handler

--- src/messages/emas.js
+++ src/messages/emas.js
@@ -2,17 +2,17 @@ const axios = require('axios').default;
 const cheerio = require('cheerio');
 const channel = require('../enum/discord-channel');
 
-module.exports = {
-    route: "/emas",
-    handler: function (client) {
-        return async (req, res) => {
-            axios.get('https://www.indogold.id/harga-emas-hari-ini').then(res => {
-                let htmlData = res.data;
-                const $ = cheerio.load(htmlData);
-                let harga = $('tbody > tr.odd > td:nth-child(2)').text().replace(/\D/g, '');
-                client.channels.cache.get(channel.BOT_TEST).send(`harga emas ${harga}`);
-            }).catch(err => console.log(err.message));
-            res.send('done');
-        }
+const route = '/emas';
+const handler = function (client) {
+    return async (req, res) => {
+        axios.get('https://www.indogold.id/harga-emas-hari-ini').then(res => {
+            let htmlData = res.data;
+            const $ = cheerio.load(htmlData);
+            let harga = $('tbody > tr.odd > td:nth-child(2)').text().replace(/\D/g, '');
+            client.channels.cache.get(channel.BOT_TEST).send(`harga emas ${harga}`);
+        }).catch(err => console.log(err.message));
+        res.send('done');
     }
 }
+
+module.exports = { route, handler }
--- src/messages/sample.js
+++ src/messages/sample.js
@@ -1,8 +1,8 @@
-module.exports = {
-    route: "/sample",
-    handler: function (client) {
-        return async function (req, res) {
-            res.send('response from sample')
-        }
+const route = '/sample';
+const handler = function (client) {
+    return async function (req, res) {
+        res.send('response from sample');
     }
 }
+
+module.exports = { route, handler };
