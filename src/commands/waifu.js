6838157f2fadc11ec42cf74c09b00bcb245f3273
galaparadi <gala_koyin@yahoo.co.id>
2022-06-05 22:55:50 +0700

    FIX : make bot sending pure image (not an embed)

--- src/commands/waifu.js
+++ src/commands/waifu.js
@@ -1,19 +1,20 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { waifu, nsfwWaifu } = require('../datasource/waifu');
-const { MessageEmbed } = require('discord.js');
+const { MessageAttachment } = require('discord.js');
+const PLUS_18_CHANNEL_ID = '834629920319340574'
 
 const command = 'waifu';
 const handler = async (interaction) => {
     const option = interaction.options.data[0] || { value: { name: false } };
     let url;
     if (option.name === 'nsfw') {
+        if (interaction.channelId != PLUS_18_CHANNEL_ID) return await interaction.reply('Mohon jalankan di channel #bahas-18-plus');
         url = await nsfwWaifu();
     } else {
         url = await waifu();
     }
-    const mem = require('../utils/mem-status')();
-    const img = new MessageEmbed().setImage(url);
-    await interaction.reply({ embeds: [img] });
+    await interaction.deferReply();
+    await interaction.editReply({ files: [new MessageAttachment(url)] });
 };
 const commandBuilder = new SlashCommandBuilder().setName(command).setDescription('Get your waifu').addStringOption(option =>
     option.setName('nsfw').setDescription('NSFW waifu').setRequired(false)
