5003abdf8922fcaaaeda9021a879793d3da060ac
galaparadi <gala_koyin@yahoo.co.id>
2023-01-25 23:00:45 +0700

    ADD : mock implementation

--- src/lib/rpc-provider.js
+++ src/lib/rpc-provider.js
@@ -2,8 +2,10 @@ const fs = require('node:fs');
 const path = require('path');
 
 function provider(options) {
-    const rpcProvider = fs.readdirSync(path.join(__dirname, '..', '/rpc'))
-        .map(location => require(path.join(__dirname, '..', '/rpc', location))(options));
+    const mock = !!parseInt(process.env.NO_BBB) && !!parseInt(process.env.DEV_DB_MODE)
+    const rpcPath = mock ? '/rpc-mock' : '/rpc';
+    const rpcProvider = fs.readdirSync(path.join(__dirname, '..', rpcPath))
+        .map(location => require(path.join(__dirname, '..', rpcPath, location))(options));
     return rpcProvider;
 }
 
