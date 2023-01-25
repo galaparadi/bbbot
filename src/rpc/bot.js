ffbe492a5a59969e7e34f4e6051f10af51a409a5
galaparadi <gala_koyin@yahoo.co.id>
2023-01-25 23:01:49 +0700

    FIX : log format

--- src/rpc/bot.js
+++ src/rpc/bot.js
@@ -1,13 +1,20 @@
+const { getChannel } = require('../datasource/bbb/channel');
+
 function rpc({ client }) {
     const routerPath = '/bot';
     const functions = {
         getStats: (args, cb) => {
-            const memory = require('../utils/mem-status')();
-            cb(null, memory);
+            try {
+                const memory = require('../utils/mem-status')();
+                cb(null, memory);
+            } catch (error) {
+                console.log('rpc -> bot -> getStats')
+                cb(error);
+            }
         },
         sendMessage: (args, cb) => {
             try {
-                const { message, channel } = args;
+                const { message, channel, data } = args;
                 client.channels.cache.get(channel).send(message);
                 cb(null, message);
             } catch (error) {
