a920d77feb8952dc0a98f1b22978b56d39a4a0d7
galaparadi <gala_koyin@yahoo.co.id>
2023-01-31 23:52:14 +0700

    FIX : error return

--- src/datasource/anime-cron.js
+++ src/datasource/anime-cron.js
@@ -1,13 +1,13 @@
 const jayson = require('jayson');
-const CRONER_URL = '127.0.0.1';
-const CRONER_PORT = 5678;
+const { CRONER_URL, CRONER_PORT } = process.env;
 
 module.exports.getAnimeSchedules = () => {
     return new Promise((res, rej) => {
         const client = new jayson.client.tcp({ port: CRONER_PORT, address: CRONER_URL });
         client.request('getSchedule', [], (err, rpcRes) => {
-            if (err) rej(err);
-            res(rpcRes.result)
+            if (err) return res({ error: err });
+            if (rpcRes.error) return res({ error: rpcRes })
+            return res(rpcRes.result)
         });
     })
 
@@ -15,13 +15,13 @@ module.exports.getAnimeSchedules = () => {
 
 module.exports.addAnimeSchedule = (data) => {
     const client = new jayson.client.tcp({ port: CRONER_PORT, address: CRONER_URL });
-    const {hour, id, date} = data;
+    const { hour, id, date } = data;
     const cron = `${hour.split(':')[1]} ${hour.split(':')[0]} * * ${date}`; //parse data to cron pattern
     return new Promise((res, rej) => {
-        client.request('addSchedule', {cron, id}, (err, rpcRes) => {
-            if(err) rej(err);
-            if(!rpcRes.result) rej(rpcRes);
-            res(rpcRes);
+        client.request('addSchedule', { cron, id }, (err, rpcRes) => {
+            if (err) return res(err);
+            if (rpcRes.error) return res({ error: rpcRes });
+            return res(rpcRes);
         });
     })
 
@@ -29,8 +29,9 @@ module.exports.addAnimeSchedule = (data) => {
 
 module.exports.removeAnimeSchedule = (id) => {
     const client = new jayson.client.tcp({ port: CRONER_PORT, address: CRONER_URL });
-    client.request('destroyCron', {id}, (err, rpcRes) => {
-        console.log(err);
-        console.log(rpcRes);
+    client.request('destroyCron', { id }, (err, rpcRes) => {
+        if (err) return res(err);
+        if (rpcRes.error) return res({ error: rpcRes });
+        return res(rpcRes);
     });
 }
